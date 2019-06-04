c1690f2eb7a3e956df0764eb767b4f886eb8692c
DanielJDA <35784183+DanielJDA@users.noreply.github.com>
2019-06-04 09:59:17 -0600

    confi

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
   description: 'Netlify + VuePress',
   themeConfig: {
     docsDir: 'docs',
-    repo: 'andreliem/vuepress-netlify-cms',
+    repo: 'spotdevelopers/handbook',
     sidebar: [
       '/',
       '/welcome',
